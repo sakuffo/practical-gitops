78ccdfb5025df1c461ea681bc4f14743b45bcfe3
Stephen Akuffo <sa.akuffo@gmail.com>
2023-03-23 11:13:15 -0400

    iter 1 done

--- chap3/main.tf
+++ chap3/main.tf
@@ -1,11 +1,27 @@
+# Setting and locking the Dependencies
 
-resource "aws_instance" "apache2_server" {
-  ami = "ami-0fd2c44049dd805b8"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.10"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+# AWS Provider configuration
+provider "aws" {
+  region = "us-east-1"
+}
+
+# AWS EC2 Resource creation
+resource "aws_instance" "apache2_servef" {
+  ami           = "ami-0fd2c44049dd805b8"
   instance_type = "t2.micro"
-  user_data = <<EOF
-         #!/bin/bash
-         sudo apt update -y
-         sudo apt install apache2 -y
-     EOF
-      
+  user_data     = file("user_data.sh")
+  tags = {
+    "env" = "dev"
+  }
 }
